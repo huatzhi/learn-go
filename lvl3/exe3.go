c8e791c6e22f82604dafc5ef0af8084622288942
Huat Zhi <huatzhi@gmail.com>
2018-05-21 20:22:26 +0800

    section 3 exercise 1

---
+++ lvl3/exe3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//func main() {
+//	for bd := 1993; bd <= 2018; bd++ {
+//		fmt.Println(bd)
+//	}
+//}
+
+func main() {
+	bd := 1993
+	for bd <= 2018 {
+		fmt.Println(bd)
+		bd++
+	}
+}
